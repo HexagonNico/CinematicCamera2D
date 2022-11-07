d784c5bb1958c023c6b51d30a9f9a46fa4953cc5
Nico <nico.hex6@gmail.com>
2022-11-07 17:59:01 +0100

    Backporting of features from 2.0

--- addons/cinematic_camera/scripts/camera_data.gd
+++ addons/cinematic_camera/scripts/camera_data.gd
@@ -3,36 +3,50 @@ class_name CameraData2D, "res://addons/cinematic_camera/icons/camera_data.svg"
 extends Node2D
 
 
-## Contains parameters used by a CinematicCamera2D
-## Works as a logic camera to be used in combination with a CinematicCamera2D
+## Contains parameters used by a CinematicCamera2D.
+## Works as a logic camera to be used in combination with a CinematicCamera2D.
 
 
-## Path to the node that the camera should follow
-export var _follow_node: NodePath
-## The node that this camera follows
+## Path to the node that the camera should follow. If this is set to a Node2D,
+## the camera will keep the same position as this node. If this is unset,
+## the camera will keep the same position as the virtual camera.
+export var _follow_node: NodePath setget _set_follow_node
+## Reference to the node that the camera should follow. If this is set to a Node2D,
+## the camera will keep the same position as this node. If this is unset,
+## the camera will keep the same position as the virtual camera.
 onready var follow_node: Node = get_node_or_null(_follow_node) setget set_follow_node
 
-## Camera offset
+## Camera offset. The offset of the cinematic camera will transition smoothly
+## to this value, according to its 'smoothing_speed'.
 export var offset: Vector2 = Vector2.ZERO setget set_offset
 
-## Camera zoom
+## Camera zoom. The zoom of the cinematic camera will transition smoothly
+## to this value, according to its 'smoothing_speed'.
 export var zoom: Vector2 = Vector2.ONE setget set_zoom
 
-## Smoothing speed
+## Smoothing speed. Used as the position, offset and zoom smoothing speed.
 export var smoothing_speed: float = 5.0 setget set_smoothing_speed
 
-## Left limit of the camera
+## Left limit of the camera. Note that this does not affect the limit
+## of the actual camera to allow for transitions between cameras with
+## different limits.
 export var limit_left: int = -10000000 setget set_limit_left
-## Top limit of the camera
+## Top limit of the camera. Note that this does not affect the limit
+## of the actual camera to allow for transitions between cameras with
+## different limits.
 export var limit_top: int = -10000000 setget set_limit_top
-## Right limit of the camera
+## Right limit of the camera. Note that this does not affect the limit
+## of the actual camera to allow for transitions between cameras with
+## different limits.
 export var limit_right: int = 10000000 setget set_limit_right
-## Bottom limit of the camera
+## Bottom limit of the camera. Note that this does not affect the limit
+## of the actual camera to allow for transitions between cameras with
+## different limits.
 export var limit_bottom: int = 10000000 setget set_limit_bottom
 
 
-## Draw the canvas item
-## Used while in the editor to draw gizmos
+## Draw the canvas item.
+## Used while in the editor to draw gizmos.
 func _draw() -> void:
 	if Engine.editor_hint:
 		draw_multiline(PoolVector2Array([
@@ -43,8 +57,8 @@ func _draw() -> void:
 		]), Color.yellow)
 
 
-## Updates the position of the given camera
-## Takes this camera's limits into account
+## Updates the position of the given camera.
+## Takes this camera's limits into account.
 func update_position(camera: Node2D) -> void:
 	# Update camera position
 	if is_instance_valid(follow_node) and follow_node is Node2D:
@@ -62,7 +76,7 @@ func set_follow_node(value: Node) -> void:
 	follow_node = value
 
 
-## Setter function for 'zoom'
+## Setter function for 'offset'
 func set_offset(value: Vector2) -> void:
 	offset = value
 
@@ -107,3 +121,10 @@ func set_limit_bottom(value: int) -> void:
 	limit_bottom = value
 	if Engine.editor_hint:
 		update()
+
+
+## Setter function for '_follow_node'
+func _set_follow_node(value: NodePath):
+	_follow_node = value
+	if Engine.editor_hint:
+		follow_node = get_node_or_null(_follow_node)
--- addons/cinematic_camera/scripts/cinematic_camera.gd
+++ addons/cinematic_camera/scripts/cinematic_camera.gd
@@ -8,7 +8,7 @@ extends Camera2D
 
 
 ## Node path to the camera metadata node to use
-export var _camera_data: NodePath
+export var _camera_data: NodePath setget _set_camera_data
 ## Reference to camera metadata node
 ## Change this value to transition to another camera
 onready var camera_data: CameraData2D = get_node_or_null(_camera_data) setget set_camera_data
@@ -19,6 +19,10 @@ func _process(delta: float) -> void:
 	if is_instance_valid(camera_data):
 		camera_data.update_position(self)
 		smoothing_speed = camera_data.smoothing_speed
+		if camera_data.zoom.x != 0.0:
+			zoom.x = lerp(zoom.x, camera_data.zoom.x, delta * smoothing_speed)
+		if camera_data.zoom.y != 0.0:
+			zoom.y = lerp(zoom.y, camera_data.zoom.y, delta * smoothing_speed)
 		zoom = lerp(zoom, camera_data.zoom, delta * smoothing_speed)
 		offset = lerp(offset, camera_data.offset, delta * smoothing_speed)
 
@@ -26,3 +30,10 @@ func _process(delta: float) -> void:
 ## Setter function for 'camera_data'
 func set_camera_data(value: CameraData2D) -> void:
 	camera_data = value
+
+
+## Setter function for '_camera_data'
+func _set_camera_data(value: NodePath) -> void:
+	_camera_data = value
+	if Engine.editor_hint:
+		camera_data = get_node_or_null(_camera_data)
