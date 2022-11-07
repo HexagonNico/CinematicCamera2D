5b6dd20ce7829bd56460925c29e011bd8777c9c2
Nico <nico.hex6@gmail.com>
2022-11-07 21:41:34 +0100

    Updated icons

--- addons/cinematic_camera/scripts/cinematic_camera.gd
+++ addons/cinematic_camera/scripts/cinematic_camera.gd
@@ -3,18 +3,18 @@ class_name CinematicCamera2D, "res://addons/cinematic_camera/icons/cinematic_cam
 extends Camera2D
 
 
-## Cinematic camera 2D node
-## Uses a CameraData2D to create smooth transitions between cameras
+## Cinematic camera 2D node.
+## Uses a CameraData2D to create smooth transitions between cameras.
 
 
-## Node path to the camera metadata node to use
+## Node path to the camera metadata node to use.
 export var _camera_data: NodePath setget _set_camera_data
-## Reference to camera metadata node
-## Change this value to transition to another camera
+## Reference to camera metadata node.
+## Change this value to transition to another camera.
 onready var camera_data: CameraData2D = get_node_or_null(_camera_data) setget set_camera_data
 
 
-## Called every frame
+## Called every frame.
 func _process(delta: float) -> void:
 	if is_instance_valid(camera_data):
 		camera_data.update_position(self)
