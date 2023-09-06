279fea884f4f6ac251723edff0075857088ca501
Nico <nico.hex6@gmail.com>
2023-09-06 21:23:21 +0200

    Updated scripts

---
+++ addons/virtual_camera_2d/scripts/cinematic_camera_2d.gd
@@ -0,0 +1,40 @@
+class_name CinematicCamera2D
+extends Camera2D
+
+
+## Cinematic camera node for 2D scenes.
+## Uses a VirtualCamera2D to create transitions between cameras.
+
+
+## The node that this camera is supposed to follow.
+## The camera's global position will be set to this node's global position every frame.
+@export var follow_node: Node2D = null
+## The current virtual camera node. A scene may have several virtual cameras, but only needs one
+## cinematic camera. Update this value to transition between different cameras.
+@export var virtual_camera: VirtualCamera2D = null
+## The speed at which the transitions take place. Used by zoom and offset.
+@export var transition_speed: float = 1.0
+
+
+# Called every frame.
+func _process(delta: float) -> void:
+	# Update the camera if a virtual camera was assigned
+	if is_instance_valid(virtual_camera):
+		# Update zoom and offset
+		zoom = zoom.move_toward(virtual_camera.zoom, delta * max(0.0, transition_speed))
+		offset = offset.move_toward(virtual_camera.offset, delta * max(0.0, transition_speed))
+		# Clamp camera position within bounds
+		var half_bounds := get_viewport_rect().size / zoom / 2.0
+		var top_left := Vector2(virtual_camera.limit_left, virtual_camera.limit_top)
+		var bottom_right := Vector2(virtual_camera.limit_right, virtual_camera.limit_bottom)
+		top_left += virtual_camera.global_position + half_bounds - offset
+		bottom_right += virtual_camera.global_position - half_bounds - offset
+		# Follow the follow node if assigned
+		if is_instance_valid(follow_node):
+			global_position = follow_node.global_position.clamp(top_left, bottom_right)
+		# Use camera position if no follow node is assigned
+		else:
+			global_position = global_position.clamp(top_left, bottom_right)
+	# Use the same position as the follow node if no virtual camera is assigned
+	elif is_instance_valid(follow_node):
+		global_position = follow_node.global_position
---
+++ addons/virtual_camera_2d/scripts/virtual_camera_2d.gd
@@ -0,0 +1,44 @@
+@tool
+class_name VirtualCamera2D
+extends Node2D
+
+
+## Node that represents a virtual camera. A scene may have several virtual cameras, one of them
+## can be assigned to a CinematicCamera2D node to make the camera copy virtual camera's properties.
+
+
+## The camera's relative offset.
+@export var offset := Vector2.ZERO
+
+## The camera's zoom.
+@export var zoom := Vector2.ONE
+
+@export_group("Limit", "limit_")
+## Left scroll limit in pixels. The camera stops moving when reaching this value.
+@export var limit_left: int = -10000000
+## Top scroll limit in pixels. The camera stops moving when reaching this value.
+@export var limit_top: int = -10000000
+## Right scroll limit in pixels. The camera stops moving when reaching this value.
+@export var limit_right: int = 10000000
+## Bottom scroll limit in pixels. The camera stops moving when reaching this value.
+@export var limit_bottom: int = 10000000
+
+@export_group("Editor", "editor_")
+## If true, draws the camera's limits rectangle in the editor.
+@export var editor_draw_limits := true
+
+
+# Called when CanvasItem has been requested to redraw.
+func _draw() -> void:
+	if Engine.is_editor_hint():
+		# Draw limits while in the editor
+		if editor_draw_limits:
+			var rect := Rect2(limit_left, limit_top, limit_right - limit_left, limit_bottom - limit_top)
+			draw_rect(rect, Color.YELLOW, false)
+
+
+# Called every frame.
+func _process(_delta: float) -> void:
+	if Engine.is_editor_hint():
+		# Needs to be called every frame in case the node is dragged in the editor
+		queue_redraw()
